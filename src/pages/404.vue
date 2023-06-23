a281aac872eafd6fb81d5b1d39d8a3a6eae4a976
Umid <umid-berdiev82@mail.ru>
2023-06-23 22:25:02 +0500

    fix: removed 404 page

--- src/pages/404.vue
+++
@@ -1,9 +0,0 @@
-<script setup lang="ts"></script>
-
-<template>
-  <AppLayout>
-    <div class="mt-5">
-      <HelloWorld />
-    </div>
-  </AppLayout>
-</template>
--- typed-router.d.ts
+++ typed-router.d.ts
@@ -40,6 +40,7 @@ import type {
 declare module 'vue-router/auto/routes' {
   export interface RouteNamedMap {
     '/': RouteRecordInfo<'/', '/', Record<never, never>, Record<never, never>>,
+    '/404': RouteRecordInfo<'/404', '/404', Record<never, never>, Record<never, never>>,
     '/todo': RouteRecordInfo<'/todo', '/todo', Record<never, never>, Record<never, never>>,
     '/todo/': RouteRecordInfo<'/todo/', '/todo', Record<never, never>, Record<never, never>>,
   }
